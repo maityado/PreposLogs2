2bf24adeeaf428ad2259fa76a829d12a0b0ad8b3
Adomaity Galeano Torres <adounicor@gmail.com>
2019-05-23 20:44:04 +0000

    guardando Temas

---
+++ leccion2/src/bin/models/Temas.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Temas = new Schema({
+  id: String,
+  descripcion: String,
+  nombre: String,
+
+
+var Temas = mongoose.model("Temas", SongSchema);
+module.exports = Temas;
